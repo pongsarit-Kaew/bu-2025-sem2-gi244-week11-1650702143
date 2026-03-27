53c76ecb94513a7aa3e7f3983cc13e335a1e5423
pongsarit-Kaew <162525812+pongsarit-Kaew@users.noreply.github.com>
2026-03-27 16:50:35 +0700

    W11

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,20 @@
 using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+    public Transform focalPoint;
 
     private Rigidbody rb;
 
     private InputAction moveAction;
     private InputAction smashAction;
     private InputAction breakAction;
+
+    public bool hasPowerUp = false;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,6 +29,49 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var move = moveAction.ReadValue<Vector2>();
+        rb.AddForce(move.y * speed * focalPoint.forward);
+        if (breakAction.IsPressed())
+        {
+            rb.linearVelocity = Vector3.zero;
+        }
+     
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (hasPowerUp == true)
+            {
+                var rb = collision.gameObject.GetComponent<Rigidbody>();
+                var dir = collision.transform.position - transform.position;
+                rb.AddForce(100 * dir.normalized, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+
+            if (countdownRountine != null)
+            {
+                StopCoroutine(countdownRountine);
+            }
+            StartCoroutine(PowerUpCountDown());
+        }
+    }
+
+    private Coroutine countdownRountine;
+
+    IEnumerator PowerUpCountDown()
+    {
+        yield return new WaitForSeconds(10f);
+        hasPowerUp = false;
 
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -5,9 +6,57 @@ public class SpawnManager : MonoBehaviour
     public Transform[] spawnPoints;
     public GameObject enemyPrefab;
 
-    void Start()
-    {
+    private Coroutine goodByeRoutine;
 
+    private void StartCoroutine(IEnumerable enumerable)
+    {
+        throw new System.NotImplementedException();
     }
 
+    IEnumerable SpawnRoutine()
+    {
+        yield return new WaitForSeconds(5);
+        while (true)
+        {
+            RandomSpawn();
+            yield return new WaitForSeconds(3);
+        }
+    }
+
+    void RandomSpawn()
+    {
+        var index = Random.Range(0, spawnPoints.Length);
+        var spawn = spawnPoints[index];
+        Instantiate(enemyPrefab, spawn.position, Quaternion.identity);
+    }
+
+    IEnumerable Goodbye()
+    {
+        while(true)
+        {
+            Debug.Log("Bye" + Time.frameCount + " " + Time.deltaTime);
+            //yield return new WaitForSeconds(1);
+            yield return null;
+
+            //StartCoroutine(Hello());
+
+            yield return Hello();
+        }
+    }
+
+    IEnumerator Hello()
+    {
+        Debug.Log("Hello" + Time.frameCount);
+        Debug.Log("Hello" + Time.frameCount);
+        Debug.Log("Hello" + Time.frameCount);
+        yield return null;
+        Debug.Log("Hello" + Time.frameCount);
+        yield return null;
+        Debug.Log("Hello" + Time.frameCount);
+        yield return null;
+        yield return null;
+        yield return null;
+        yield return null;
+        Debug.Log("Hello" + Time.frameCount);
+    }
 }
